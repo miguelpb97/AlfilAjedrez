4e0ac0a71085bcc1b8d19329e4451e254c708121
miguelpb97 <miguelpb97@localhost>
2020-11-21 02:13:02 +0100

    ejercicio8

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -14,6 +14,14 @@ public class Posicion {
 		setColumna(columna);
 	}
 	
+	public Posicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
+		}
+		setFila(posicion.getFila());
+		setColumna(posicion.getColumna());
+	}
+	
 	private void setFila(int fila) {
 		if (fila < 1 || fila > 8) {
 			throw new IllegalArgumentException("ERROR: Fila no válida.");
