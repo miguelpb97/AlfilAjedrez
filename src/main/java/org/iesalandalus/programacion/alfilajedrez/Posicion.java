5da50acd2cbbd77b8fe16b4e076b868667c2029e
miguelpb97 <miguelpb97@localhost>
2020-11-21 02:11:47 +0100

    ejercicio6

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -5,4 +5,26 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	public int getFila() {
+		return fila;
+	}
+	
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+	
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+		this.columna = columna;
+	}
+	
 }
