a3aee5c62dff6c9040fd31b53e92c5bb144f548c
miguelpb97 <miguelpb97@localhost>
2020-11-27 06:02:11 +0100

    ejercicio12

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -2,7 +2,23 @@ package org.iesalandalus.programacion.alfilajedrez;
 
 public class Alfil {
 
-	private Color color;
 	private Posicion posicion;
+	private Color color;
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	private void setColor(Color color) {
+		this.color = color;
+	}
 	
 }
