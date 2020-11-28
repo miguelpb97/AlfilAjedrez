7ee64245bf5e2141970f913bd4478c2a30617f4c
miguelpb97 <miguelpb97@localhost>
2020-11-28 03:54:39 +0100

    ejercicio14

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -9,6 +9,19 @@ public class Alfil {
 		color = Color.NEGRO;
 		posicion = new Posicion(8,'f');
 	}
+	
+	public Alfil(Color color) {
+		if (color == null)
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		if (color == Color.NEGRO) {
+			color = Color.NEGRO;
+			posicion = new Posicion(8,'f');
+		} else if (color == Color.BLANCO) {
+			color = Color.BLANCO;
+			posicion = new Posicion(1,'f');}
+		setColor(color);	
+	}
+	
 
 	public Posicion getPosicion() {
 		return posicion;
