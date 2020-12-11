8d208f089c60e9d55763d251245d8accc95f0ec8
miguelpb97 <miguelpb97@localhost>
2020-12-11 21:22:39 +0100

    ejercicio 19.2

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -2,9 +2,13 @@ package org.iesalandalus.programacion.alfilajedrez;
 
 public class MainApp {
 	
-	public void mostrarAlfil() {
-		Alfil alfil1 = new Alfil();
-		System.out.println(alfil1);
+	private static void mostrarAlfil() {
+		Alfil alfil = new Alfil();
+		System.out.println(alfil);
+	}
+	
+	private static void mostrarMenu() {
+
 	}
 	
 }
